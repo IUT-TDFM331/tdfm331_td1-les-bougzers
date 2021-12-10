eabd134a442b4bb79da88c625bbd5e45a88a0faa
fertilati julien <julien.fertilati@etu.unice.fr>
2021-12-10 11:08:31 +0100

    First

--- src/test/java/fr/iut/tdfm331/td1/model/MeetingTest.java
+++ src/test/java/fr/iut/tdfm331/td1/model/MeetingTest.java
@@ -6,7 +6,10 @@ import org.junit.jupiter.api.BeforeEach;
 import static org.junit.jupiter.api.Assertions.*;
 
 class MeetingTest {
-    ///PQZFQZF
+
+
+//paspd
+
     @BeforeEach
     void setUp() {
     }
