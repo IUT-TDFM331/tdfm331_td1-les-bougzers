3e89917bd4d7e12988e229778dfccfa5db363622
fertilati julien <julien.fertilati@etu.unice.fr>
2021-12-10 10:55:20 +0100

    First

--- src/main/java/fr/iut/tdfm331/td1/model/Meeting.java
+++ src/main/java/fr/iut/tdfm331/td1/model/Meeting.java
@@ -5,7 +5,8 @@ import java.util.List;
 /**
  * Model object representing a Meeting
  */
-public class Meeting {
+public
+class Meeting {
 
     private final String objectMeeting;
     private final String meetingRoom;
---
+++ src/test/java/fr/iut/tdfm331/td1/model/MeetingTest.java
@@ -0,0 +1,17 @@
+package fr.iut.tdfm331.td1.model;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MeetingTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+}
