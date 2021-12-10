8ec7745c0e0cddeb1e576b88f2f4e994b7dc40eb
demont samuel <samuel.demont@etu.unice.fr>
2021-12-10 11:31:51 +0100

    yazid

---
+++ src/test/java/fr/iut/tdfm331/td1/YazidOeufTest.java
@@ -0,0 +1,13 @@
+package fr.iut.tdfm331.td1;
+
+import org.junit.Test;
+import org.junit.Assert;
+
+
+public class YazidOeufTest {
+    @Test
+    void testIfOeuf(){
+
+        Assert.assertEquals("yazid","oeuf");
+    }
+}
